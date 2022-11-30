fccf2dae85da75fb4568a0289e2f209a3d7894e3
udesh98 <um4me.98@gmail.com>
2022-11-30 13:39:33 +0530

    Signup and Login completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./components/Main";
-import Signup from "./components/Singup";
+import Signup from "./components/Signup";
 import Login from "./components/Login";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
 
 function App() {
 	const user = localStorage.getItem("token");
@@ -11,7 +13,12 @@ function App() {
 			{user && <Route path="/" exact element={<Main />} />}
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
+			{user && <Route path="/home" exact element={<Home />} />}
+			{user && <Route path="/projects" exact element={<Projects />} />}
+			
 			<Route path="/" element={<Navigate replace to="/login" />} />
+			<Route path="/home" element={<Navigate replace to="/login" />} />
+			<Route path="/projects" element={<Navigate replace to="/login" />} />
 		</Routes>
 	);
 }
--- frontend/src/components/NavControl/index.jsx
+++ frontend/src/components/NavControl/index.jsx
@@ -1,18 +1,24 @@
+import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 
 const Main = () => {
 
 	return (
 		<section className={styles.menu}>
+			<Link to="/home">
 				<button className={styles.menu_item}>
 					Home
 				</button>
+			</Link>
+			<Link to="/projects">
 				<button className={styles.menu_item}>
-					Tasks
+					Projects
 				</button>
-				<button className={styles.menu_item}>
-					About
-				</button>
+			</Link>
+			
+			<button className={styles.menu_item}>
+				About
+			</button>
 		</section>
 	);
 };
